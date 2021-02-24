87a076840e2211e7eb528dbb1818e7ee9a56f8e9
Lucas <carvalho1104@hotmail.com>
2021-02-24 19:15:51 -0300

    main

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ container.addEventListener('mouseenter', (e) =>{
     card.style.transition = 'none';
 
     title.style.transform = 'translateZ(150px)';
-    sneaker.style.transform = 'translateZ(200px) rotateZ(-45deg)';
+    sneaker.style.transform = 'translateZ(100px) rotateZ(-30deg)';
     description.style.transform = 'translateZ(125px)';
     sizes.style.transform = 'translateZ(100px)';
     purchase.style.transform ='translateZ(75px)';
